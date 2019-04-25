5a99de184583f71620632015817a10e827abcdbc
Vanessa <vanessayoshida92@gmail.com>
2019-04-25 14:24:57 -0300

    Criado testes para validar numero do cartao e quantidade de numeros

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,11 +3,42 @@ const expect = chai.expect;
 const CC = require('../lib/index');
 
 describe('cardValidator()', () => {
-    it('Deveria retornar true para 5526988157883653', () => {
-        expect(CC.cardValidator(5526988157883653)).to.equal(true);
+    describe ("Verificação dos números do cartão de crédito", () => {
+        it('Deveria retornar true para a numeração: 5526988157883653', () => {
+            expect(CC.cardValidator(5526988157883653)).to.equal(true);
+        });
+
+        it('Deveria retornar true para a numeração: 371144343967748', () => {
+            expect(CC.cardValidator(371144343967748)).to.equal(true);
+        });
+
+        it('Deveria retornar true para a numeração: 4389351373448893', () => {
+            expect(CC.cardValidator(4389351373448893)).to.equal(true);
+        });
+        
+        it('Deveria retornar false para a numeração: 1234567891234567', () => {
+            expect(CC.cardValidator(1234567891234567)).to.equal(false);
+        });
+
+        it('Deveria retornar false para a numeração: 4337689576382348', () => {
+            expect(CC.cardValidator(4337689576382348)).to.equal(false);
+        });
     });
-    
-    it('Deveria retornar false para 1234567891234567', () => {
-        expect(CC.cardValidator(1234567891234567)).to.equal(false);
+    describe ("Verificação da quantidade de números", () => {
+        it('Deveria retornar true para 14 números', () => {
+            expect(CC.cardValidator(30354535753490)).to.equal(true);
+        });
+
+        it('Deveria retornar true para 16 números', () => {
+            expect(CC.cardValidator(5526988157883653)).to.equal(true);
+        });
+
+        it('Deveria retornar false para 11 números', () => {
+            expect(CC.cardValidator(55269881578)).to.equal(false);
+        });
+
+        it('Deveria retornar false para 20 números', () => {
+            expect(CC.cardValidator(55269881578836531342)).to.equal(false);
+        });
     });
 });
