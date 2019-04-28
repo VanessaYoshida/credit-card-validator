f8b6057c27eeee0928a1dc353ccaacf7489859eb
Vanessa <vanessayoshida92@gmail.com>
2019-04-28 13:08:06 -0300

    Arrumei as regras do ESLINT

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,25 +29,86 @@ module.exports = {
             'error',
             'always'
         ],
-        'id-length': 2,
-        'camelcase': 2,
-        'keyword-spacing': 2,
-        'space-infix-ops': 2,
-        'comma-spacing': 2,
-        'space-before-blocks': 2,
-        'spaced-comment': 2,
-        'key-spacing': 2,
-        'no-multi-spaces': 2,
-        'space-in-parens': 2,
-        'space-before-function-paren': 2,
-        'func-call-spacing': 2,
-        'no-multiple-empty-lines': 2,
-        'padded-blocks': 2,
-        'quotes': 2,
-        'no-floating-decimal': 2,
-        'object-property-newline': 2,
-        'brace-style': 2,
-        'eqeqeq': 2,
-        'no-array-constructor': 2
+        'id-length': [
+            'error',
+            {'min': 2}
+        ],
+        'camelcase': [
+            'error',
+            {'properties': 'always'}
+        ],
+        'keyword-spacing': [
+            'error', 
+            { 'after': true }
+        ],
+        'space-infix-ops': [
+            'error', 
+            { 
+                'int32Hint': false,
+                'exceptions': ['--', '++'] 
+            }
+        ],
+        'comma-spacing': [
+            'error', 
+            { before: false, after: true }
+        ],
+        'space-before-blocks': [
+            'error', 
+            'always'
+        ],
+        'spaced-comment': [
+            'error', 
+            'always'
+        ],
+        'key-spacing': [
+            'error', 
+            { 'afterColon': true }
+        ],
+        'no-multi-spaces': [
+            'error', 
+            { align: "value" }
+        ],
+        'space-in-parens': [
+            'error', 
+            'never'
+        ],
+        'space-before-function-paren': [
+            'error', 
+            'never'
+        ],
+        'func-call-spacing': [
+            'error', 
+            'never'
+        ],
+        'no-multiple-empty-lines': [
+            'error', 
+            { 'max': 0 }
+        ],
+        'padded-blocks': [
+            'error', 
+            'never'
+        ],
+        'quotes':[
+            'error',
+            'single'
+        ],
+        'no-floating-decimal': [
+            'error'
+        ],
+        'object-property-newline': [
+            'error'
+        ],
+        'brace-style': [
+            'error', 
+            '1tbs'
+        ],
+        'eqeqeq': [
+            'error', 
+            'always', 
+            { 'null': 'ignore' }
+        ],
+        'no-array-constructor': [
+            'error'
+        ]
     }
 };
